3b2a21f8ca1ac30c7d3a16c2d9ae8d8ba62b6f66
sthefany22222 <118447283+sthefany22222@users.noreply.github.com>
2024-04-25 17:11:51 -0300

    .

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-29-10")
+
+contadores[0].textContent = tempoObjetivo1;
